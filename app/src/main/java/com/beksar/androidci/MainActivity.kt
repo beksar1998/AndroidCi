9a36ad8f074cc417ae972c17f8f01134a43522d6
beksar1998 <beksar1998@mail.ru>
2021-04-22 12:50:08 +0600

    removed test

--- app/src/main/java/com/beksar/androidci/MainActivity.kt
+++ app/src/main/java/com/beksar/androidci/MainActivity.kt
@@ -1,13 +1,45 @@
 package com.beksar.androidci
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.beksar.androidci.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        binding.installTime.setOnClickListener {
+            val intent = Intent()
+            intent.setClassName(
+                BuildConfig.APPLICATION_ID,
+                "com.example.installtime.InstallTimeActivity"
+            );
+            startActivity(intent)
+        }
+
+        binding.onDemandOnly.setOnClickListener {
+            val intent = Intent()
+            intent.setClassName(
+                BuildConfig.APPLICATION_ID,
+                "com.example.ondemandonly.OnDemandActivity"
+            );
+            startActivity(intent)
+        }
+
+        binding.specific.setOnClickListener {
+            val intent = Intent()
+            intent.setClassName(
+                BuildConfig.APPLICATION_ID,
+                "com.example.specific.SpecificActivity"
+            );
+            startActivity(intent)
+        }
     }
 }
