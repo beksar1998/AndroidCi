81d9b6a3b014878e668d5095a0fdfb9b121c7f4b
beksar1998 <beksar1998@mail.ru>
2021-04-22 12:25:39 +0600

    added activities

---
+++ installtime/src/main/java/com/example/installtime/InstallTimeActivity.kt
@@ -0,0 +1,11 @@
+package com.example.installtime
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class InstallTimeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_install_time)
+    }
+}
